fd049d6fdd9338d7d13b8ceb6184f73ffca82160
Thao Dang <tdang@thoughtworks.com>
2016-02-20 10:00:57 +0800

    fix unit tests

--- server/app/src/services/authentication-service.js
+++ server/app/src/services/authentication-service.js
@@ -5,15 +5,15 @@ var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://192.168.55.10:27017/test';
 
 exports.authenticate = (username, password) => {
-    mongoClient.connect(url)
+    return mongoClient.connect(url)
         .then((db) => {
-            db.collection('users').findOne({"username": username})
+            return db.collection('users').findOne({"username": username})
         })
         .then((doc) => {
             console.log(doc);
             if (doc && doc.password === password) {
-                return Promise.resolve();
+                return Promise.resolve(true);
             }
-            return Promise.reject();
+            return Promise.reject(false);
         });
 };
--- server/app/test/services/authentication-service-tests.js
+++ server/app/test/services/authentication-service-tests.js
@@ -4,61 +4,36 @@
 require("promise.prototype.finally");
 var expect = require("chai").expect;
 var mongoClient = require('mongodb').MongoClient;
-var authenticationService = require('../../services/authentication-service');
+var authenticationService = require('../../src/services/authentication-service');
 var url = 'mongodb://192.168.55.10:27017/test';
 
-describe("A valid user", () => {
-    it("is authenticated with a valid password", (done) => {
-        theDb = null;
-        mongoClient.connect(url)
-        .then((db) => {
-            theDb = db;
-            collection = db.collection('users');
-            return collection.insertOne( {
-                "username" : "batman",
-                "password" : "IloveRobin"
-            });
-        })
-        .then((result) => {
-            console.log("Inserted a document into the users collection.");
-            return authenticationService.authenticate("batman", "IloveRobin");
-        })
-        .then(() => {
-            expect(true).is.ok;
-        })
-        .catch(() => {
-            expect(true).is.not.ok;
-        })
-        .finally(() => {
-            if(theDb){theDb.close();}
-            done();
-        });
-    });
-
-    it("is not authenticated with an invalid password", (done) => {
+describe("A valid user", function () {
+    beforeEach(function () {
         theDb = null;
         mongoClient.connect(url)
             .then((db) => {
                 theDb = db;
                 collection = db.collection('users');
-                return collection.insertOne( {
-                    "username" : "batman",
-                    "password" : "IloveRobin"
+                collection.insertOne({
+                    "username": "batman",
+                    "password": "IloveRobin"
                 });
-            })
-            .then((result) => {
-                console.log("Inserted a document into the users collection.");
-                return authenticationService.authenticate("batman", "IloveRobinhood");
-            })
-            .then(() => {
-                expect(true).is.not.ok;
-            })
-            .catch(() => {
-                expect(true).is.ok;
-            })
-            .finally(() => {
-                if(theDb){theDb.close();}
                 done();
-            });
+            }).finally(function () {
+            if (theDb) {
+                theDb.close();
+            }
+        });
+    });
+
+    it("is authenticated with a valid password", function() {
+        return authenticationService.authenticate("batman", "IloveRobin")
+            .then(function (result){expect(result).be.true});
+    });
+
+    it("is not authenticated with an invalid password", () => {
+        return authenticationService.authenticate("batman", "IloveRobinhood")
+            .then(function(result) {throw new Error("Should not authenticate user with invalid password")},
+            function (result){expect(result).be.false});
     });
 });
