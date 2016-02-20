6e607cbbcd1dc834da00e7bb3750a4a51bc956e4
Thao Dang <tdang@thoughtworks.com>
2016-02-20 10:28:44 +0800

    added console logs to help debugging snap

--- server/app/src/services/authentication-service.js
+++ server/app/src/services/authentication-service.js
@@ -5,8 +5,10 @@ var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://192.168.55.10:27017/test';
 
 exports.authenticate = (username, password) => {
+    console.log("authenticating... ", username, " password: ", password);
     return mongoClient.connect(url)
         .then((db) => {
+            console.log(connected);
             return db.collection('users').findOne({"username": username})
         })
         .then((doc) => {
